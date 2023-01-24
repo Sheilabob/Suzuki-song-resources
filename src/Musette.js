35041d9108851fba48fde4d2a9a1c994bad60cb6
Sheila <smstrahan@yahoo.com>
2023-01-23 19:06:15 -0700

    section b lh

--- src/Musette.js
+++ src/Musette.js
@@ -47,6 +47,8 @@ function Musette() {
         <audio src="assets/audio/Musette-B1-LH.mp3" controls>Your browser does not support HTML5 audio.</audio>
         <h3>Section B, phrase 2, left hand</h3>
         <audio src="assets/audio/Musette-B2-LH.mp3" controls>Your browser does not support HTML5 audio.</audio>
+        <h3>Section B, left hand</h3>
+        <audio src="assets/audio/Musette-B-LH.mp3" controls>Your browser does not support HTML5 audio.</audio>
 
  
       </div>
