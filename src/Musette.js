36aa406fb3fe748149b19655a7a6033d2ea2ff65
Sheila <smstrahan@yahoo.com>
2023-01-23 19:01:21 -0700

    section A both hands

--- src/Musette.js
+++ src/Musette.js
@@ -1,10 +1,10 @@
-import SectionA1RH from "./Musette/SectionA1RH";
 
 function Musette() {
     return (
       <div>
         <h1>Musette Resources</h1>
         <h2>Tutorial Videos</h2>
+        <p>How it works: these videos are all the same video, just bookmarked to different sections.  Watch the full video or use one of the others to skip to the section you need.</p>
         <h3>Full Video</h3>
         <iframe width="560" height="315" src="https://www.youtube.com/embed/wTiI71clTD4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
         <h3>Introduction</h3>
@@ -24,7 +24,21 @@ function Musette() {
         <h3>Whole Song</h3>
         <iframe width="560" height="315" src="https://www.youtube.com/embed/wTiI71clTD4?start=928" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
         <h2>Guided Practice</h2>
-         <SectionA1RH />
+        <p>How it works:  select the section you are practicing, get set and press play.  The section will play through 6 times in a row with a count between each time.  The first time is a "demo," a chance for you to listen and get ready, then try to play along with the rest.  IF YOU NEED, you can slow the playback down (just don't forget to speed it back up again later).  You'll know you've mastered it when you can easily play along with all five of the repeats following the demo.</p>
+        <h3>Section A, phrase 1, right hand</h3>
+        <audio src="assets/audio/Musette-RH-1a.mp3" controls>Your browser does not support HTML5 audio.</audio>
+        <h3>Section A, phrase 2, right hand</h3>
+        <audio src="assets/audio/Musette-RH-1b.mp3" controls>Your browser does not support HTML5 audio.</audio>
+        <h3>Section A, phrases 1 and 2, left hand</h3>
+        <audio src="assets/audio/Musette-LH-A.mp3" controls>Your browser does not support HTML5 audio.</audio>
+        <h3>Section A, phrase 1, both hands</h3>
+        <audio src="assets/audio/Musette-A1.mp3" controls>Your browser does not support HTML5 audio.</audio>
+        <h3>Section A, phrase 2, both hands</h3>
+        <audio src="assets/audio/Musette-A2.mp3" controls>Your browser does not support HTML5 audio.</audio>
+        <h3>Section A, both hands</h3>
+        <audio src="assets/audio/Musette-A.mp3" controls>Your browser does not support HTML5 audio.</audio>
+
+ 
       </div>
     );
   }
