597b213e76dbf58f18877b6af7a52e663e9255bd
Sheila <smstrahan@yahoo.com>
2023-01-28 17:50:31 -0700

    updated site to include goodbye to winter

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Home from './Home';
 import Musette from './Musette';
+import GoodbyeToWinter from './GoodbyeToWinter';
 import {
   BrowserRouter as Router,
   Routes,
@@ -19,8 +20,12 @@ function App() {
               <Link to="/"><h1>Home</h1></Link>
             </li>
             <li class="menu-item">
+              <Link to="/goodbyetowinter"><h1>Goodbye To Winter</h1></Link>
+            </li>
+            <li class="menu-item">
               <Link to="/musette"><h1>Musette</h1></Link>
             </li>
+
           </ul>
         </nav>
 
@@ -28,6 +33,10 @@ function App() {
             renders the first one that matches the current URL. */}
             <div class="pages">
         <Routes>
+          <Route path="/goodbyetowinter" element={ <GoodbyeToWinter />}>
+          </Route>
+
+
           <Route path="/musette" element={ <Musette />}>
            
           </Route>
