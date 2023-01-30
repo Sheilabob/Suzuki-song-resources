61e91610a4354b7a979b98d9fc4f466307cc5aba
Sheila <smstrahan@yahoo.com>
2023-01-30 16:40:21 -0700

    added some lyrics to home page

--- src/Home.js
+++ src/Home.js
@@ -5,6 +5,35 @@ function Home() {
         <h2>Spotify Playlist, Level 1</h2>
         <p>Please note: this is a "preview" list and will only play parts of the songs here.  Please follow the link to spotify to listen to the songs in full. (You will need to sign up for a free account.)</p>
         <iframe style={{borderRadius:'12px'}} src="https://open.spotify.com/embed/playlist/2pqP4buMoIHBqmWemvOcBz?utm_source=generator&theme=0" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+
+        <h2>Words for selected songs</h2>
+        <h3>Honeybee</h3>
+        <p>Honeybee, don't you dare sting me. <br />
+          Don't sting Mommy, don't sting Daddy, <br />
+          Don't sting Mommy, don't sting Daddy, <br />
+          Honeybee, don't you dare sting me. 
+        </p>
+        <h3>Cuckoo</h3>
+        <p> Cuckoo, Cuckoo, sings in the forest. <br />
+        Cuckoo, Cuckoo, sings in the woods. <br />
+        Let us be singing, let us be dancing. <br />
+        Cuckoo, Cuckoo, bedtime is here.
+        </p>
+        <h3>Lightly Row</h3>
+        <p> Lightly row, lightly row, gently down the stream we go. <br />
+        Smoothly gliding, smoothly gliding, on the silent tide we row. <br />
+        Let the wind and water be, mingled with our melody. <br />
+        Smoothly gliding, smoothly gliding, on the silent tide we row.
+        </p>
+        <h3>French Children's Song</h3>
+        <p>French children play and greet the happy morning. <br />
+        French children play and greet the happy day. <br />
+        French children sing happily in the garden. <br />
+        French children sing happily in the yard.  <br />
+        French children play and greet the happy morning. <br />
+        French children play and greet the happy day. 
+        </p>
+
       </div>
     );
   }
