85400e7956060dbfa7f65172467fb4369cb3e503
Sheila <smstrahan@yahoo.com>
2023-02-01 18:44:37 -0700

    homepage accordion 2

--- src/Home.js
+++ src/Home.js
@@ -1,19 +1,41 @@
+import { useState } from "react"
+
 function Home() {
+  const [expand, setExpand] = useState(false)
+  const [expand2, setExpand2] = useState(false)
+  const [expand3, setExpand3] = useState(false)
+
+
+  const handleClick = () => setExpand(!expand)
+  const handleClick2 = () => setExpand2(!expand2)
+  const handleClick3 = () => setExpand3(!expand3)
+
     return (
       <div>
-        <section>
-        <h1>Suzuki Song Resource Site</h1>
-        <h2>Spotify Playlist, Level 1</h2>
+                <h1>Suzuki Song Resource Site</h1>
+
+        <section className="accordion">
+        <div className="accordion-header"><h2>Spotify Playlist, Level 1</h2><h1 className="plus" aria-expanded={expand} onClick={handleClick}>+</h1>
+            <h1 className="minus" aria-expanded={!expand} onClick={handleClick}>-</h1>
+          </div>
+          <div className="accordion-content" aria-expanded={!expand}>
         <p>Please note: this is a "preview" list and will only play parts of the songs here.  Please follow the link to spotify to listen to the songs in full. (You will need to sign up for a free account.)</p>
         <iframe style={{borderRadius:'12px'}} src="https://open.spotify.com/embed/playlist/2pqP4buMoIHBqmWemvOcBz?utm_source=generator&theme=0" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+        </div>
 </section>
-<section>
+<section className="accordion">
+<div className="accordion-header">
+          <h2>YouTube Resources, Level 1</h2>
+          <h1 className="plus" aria-expanded={expand2} onClick={handleClick2}>+</h1>
+            <h1 className="minus" aria-expanded={!expand2} onClick={handleClick2}>-</h1>
+          </div>
+          <div className="accordion-content" aria-expanded={!expand2}>
     <h2>Falling Bars Video (full level video, songs are in order, bookmarks in description, Note: the Twinkle Variations are not included)</h2>
     <iframe width="560" height="315" src="https://www.youtube.com/embed/_1Rb5z0WT0g" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-</section>
-<section>
+
           <h2>Link to Turk Piano Studios: full playlist for level 1, songs are in order</h2>
           <iframe width="560" height="315" src="https://www.youtube.com/embed/videoseries?list=PLU13ELqa_CK9uvoLTyx0EQY-RbzS0xLd9" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+          </div>
         </section>
         <h2>Words for selected songs</h2>
         <h3>Honeybee</h3>
